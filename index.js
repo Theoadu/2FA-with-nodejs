0c66d8fa567611526021f029f419b6f28ed8c013
Theophilus E.K Adukpo <theoadu@gmail.com>
2020-11-12 20:00:27 +0000

    Added DB

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from 'express'
-import uuid from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
 import speakeasy from 'speakeasy'
 import { JsonDB } from 'node-json-db';
-import { Config } from 'node-json-db/dist/lib/JsonDBConfig'
+import { Config } from 'node-json-db/dist/lib/JsonDBConfig.js'
 
 const app = express();
+express.json()
 
-app.get('/',(req, res)=>{
+const dbConfig = new Config("myDatabase", true, false, '/')
+
+const db = new JsonDB(dbConfig)
+
+app.get('/api',(req, res)=>{
     res.json({message: 'Welcome to 2FA'})
 })
 const PORT = 9000
-app.listen(PORT, console.log(`Server started at port ${PORT}`))
+app.listen(PORT, ()=>console.log(`Server started at port ${PORT}`))
